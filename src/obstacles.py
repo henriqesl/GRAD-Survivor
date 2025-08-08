4ec4cac893e9ce7dd3824217a299356de2239876
Júnior Cruz <juniorcruzdev7@gmail.com>
2025-08-08 00:04:20 -0300

    Dá pra usar

--- src/obstacles.py
+++ src/obstacles.py
@@ -4,35 +4,47 @@ pygame.init()
 pygame.display.set_caption("GRAD-Survivor")
 screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+
 map = pygame.image.load('assets/images/mapa1.0.png')
 
-# Definindo os retângulos de contato
-parede1 = pygame.Rect((0, 0, 4, 600))
-parede2 = pygame.Rect((796, 0, 4, 600))
-parede3 = pygame.Rect((0, 0, 800, 4))
-parede4 = pygame.Rect((0, 595, 800, 4))
+#Daqui pra cima morre!
 
-porta1 = pygame.Rect((0, 56, 57, 3))
-macaneta1 = pygame.Rect((40, 51, 12, 5))
+class Obstaculos:
+    def __init__(self, obstaculos):
+        self.parede1 = pygame.Rect((0, 0, 4, 600))
+        self.parede2 = pygame.Rect((796, 0, 4, 600))
+        self.parede3 = pygame.Rect((0, 0, 800, 4))
+        self.parede4 = pygame.Rect((0, 595, 800, 4))
 
-porta2 = pygame.Rect((743, 53, 53, 3))
-macaneta2 = pygame.Rect((746, 48, 12, 5))
+        self.porta1 = pygame.Rect((0, 56, 57, 3))
+        self.macaneta1 = pygame.Rect((40, 51, 12, 5))
 
-porta3 = pygame.Rect((0, 493, 55, 3))
-macaneta3 = pygame.Rect((40, 488, 12, 5))
+        self.porta2 = pygame.Rect((743, 53, 53, 3))
+        self.macaneta2 = pygame.Rect((746, 48, 12, 5))
 
-porta4 = pygame.Rect((744, 492, 55, 3))
-macaneta4 = pygame.Rect((747, 487, 12, 5))
+        self.porta3 = pygame.Rect((0, 493, 55, 3))
+        self.macaneta3 = pygame.Rect((40, 488, 12, 5))
 
-mesa1 = pygame.Rect((0, 370, 185, 80))
-mesa2 = pygame.Rect((0, 173, 185, 80))
-mesa3 = pygame.Rect((620, 169, 185, 80))
-mesa4 = pygame.Rect((620, 375, 185, 80))
-mesa5 = pygame.Rect((464, 33, 88, 52))
+        self.porta4 = pygame.Rect((744, 492, 55, 3))
+        self.macaneta4 = pygame.Rect((747, 487, 12, 5))
 
-tv = pygame.Rect((308, 4, 185, 4))
+        self.mesa1 = pygame.Rect((0, 369, 186, 82))
+        self.mesa2 = pygame.Rect((0, 173, 186, 80))
+        self.mesa3 = pygame.Rect((619, 169, 185, 80))
+        self.mesa4 = pygame.Rect((619, 374, 185, 81))
+        self.mesa5 = pygame.Rect((464, 33, 88, 52))
 
-obstaculos = [parede1, parede2, parede3, parede4, porta1, porta2, porta3, porta4, macaneta1, macaneta2, macaneta3, macaneta4, mesa1, mesa2, mesa3, mesa4, mesa5, tv]
+        self.tv = pygame.Rect((308, 4, 185, 4))
+
+
+    def obstaculos(self):
+
+        return (self.parede1, self.parede2, self.parede3, self.parede4, self.porta1, self.porta2, self.porta3, self.porta4, self.macaneta1, self.macaneta2, self.macaneta3, self.macaneta4, self.mesa1, self.mesa2, self.mesa3, self.mesa4, self.mesa5, self.tv)
+    
+        
+
+#Daqui pra baixo some
+
 
 # ====== JOGADOR (RETÂNGULO AZUL) ======
 player = pygame.Rect(100, 100, 30, 30)  # (x, y, largura, altura)
@@ -59,7 +71,7 @@ while True:
     if keys[pygame.K_DOWN]:
         player.y += velocidade
 
-    for obstaculo in obstaculos:
+    for obstaculo in Obstaculos(None).obstaculos():
         if player.colliderect(obstaculo):
             player.x = prev_x
             player.y = prev_y
