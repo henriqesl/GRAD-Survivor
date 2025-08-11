75562930cb006c78c657afcf3733c3cf73c997be
Tiago Mattos <tiagolmattos06@gmail.com>
2025-08-10 22:16:54 -0300

    Update tela_incial.py
    tela inicial

---
+++ src/tela_incial.py
@@ -0,0 +1,16 @@
+import pygame
+from settings import WINDOW_WIDTH, WINDOW_HEIGHT
+
+class TelaInicial:
+    def __init__(self, screen):
+        self.screen = screen
+        self.fundo = pygame.image.load("assets/images/inicio.png").convert()
+        self.botao_img = pygame.image.load("assets/images/play.png").convert_alpha()
+        self.botao_rect = self.botao_img.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 150))
+
+    def desenhar(self):
+        self.screen.blit(self.fundo, (0, 0))
+        self.screen.blit(self.botao_img, self.botao_rect)
+
+    def verificar_clique(self, pos_mouse):
+        return self.botao_rect.collidepoint(pos_mouse)
